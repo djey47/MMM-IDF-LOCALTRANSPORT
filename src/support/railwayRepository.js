d0e23056b2b8a7c77bbc2d3c43bafaa400f67d1a
djey <j.canler@gmail.com>
2017-06-28 00:03:37 +0200

    feature(api): use web service to resolve stations by UIC

--- src/support/railwayRepository.js
+++ src/support/railwayRepository.js
@@ -1,51 +1,38 @@
-const stationRepository = require('../../api/station-repository.json');
 const unirest = require('unirest');
 
 /**
- * @returns station label having provided UIC code, or null if it does not exist
+ * @returns first station info matching provided query (label or UIC), or null if it does not exist
  */
-const getStationByUIC = function(uic) {
-  const normalizedCode = `00${uic}`;
-  const occurences = stationRepository
-    .filter(station => station.fields.uic === normalizedCode);
-
-  if (occurences.length) {
-    return occurences[0].fields.intitule_gare;
-  }
-
-  return null;
-};
-
-/**
- * @returns first station info matching provided label, or null if it does not exist
- */
-const getStationInfoByLabel = function(label, config) {
+const getStationInfo = function(query, config) {
   const { sncfApiUrl, debug } = config;
-  const url = encodeURI(`${sncfApiUrl}search?q=${label}&dataset=sncf-gares-et-arrets-transilien-ile-de-france&sort=libelle`);
+  const url = encodeURI(`${sncfApiUrl}search?q=${query}&dataset=sncf-gares-et-arrets-transilien-ile-de-france&sort=libelle`);
 
-  let stationInfo = null;
-  unirest.get(url)
+  const callPromise = (url) => new Promise((resolve, reject) => unirest.get(url)
     .header({
       'Accept': 'application/json;charset=utf-8',
     })
-    .end(function(response) {
+    .end((response) => {
       if (response && response.body && response.body.records.length) {
-        stationInfo = response.body.records[0].fields;
         if (debug) {
-          console.log(`** Station info found for label ${label}`);
+          console.log(`** Station info found for '${query}'`);
           console.dir(stationInfo);
         }        
+        resolve(response.body.records[0].fields);
       } else {
         if (debug) {
-          console.log(`** No station info found for label ${label}`);
+          console.log(`** No station info found for '${query}'`);
         }
+        reject();
       }
-    });
+    }));
+
+  let stationInfo = null;
+  callPromise(url)
+    .then(value => stationInfo = value);
 
   return stationInfo;  
 };
 
 module.exports = {
-  getStationByUIC,
-  getStationInfoByLabel,
+  getStationInfo,
 };
--- src/support/railwayRepository.spec.js
+++ src/support/railwayRepository.spec.js
@@ -1,43 +1,17 @@
 /* @flow */
 
-import { getStationByUIC, getStationInfoByLabel } from './railwayRepository';
+import { getStationInfo } from './railwayRepository';
 
-describe('getStationByUIC function', () => {
-  it('should return proper label when uic code exists', () => {
-    // given
-    const uic = '87384008';
-    // when
-    const actual = getStationByUIC(uic);
-    // then
-    expect(actual).toEqual('Paris Saint-Lazare');
-  });
-
-  it('should return null when uic code does not exist', () => {
-    // given-when
-    const actual = getStationByUIC('');
-    // then
-    expect(actual).toBeNull();
-  });
-});
-
-describe('getStationInfoByLabel function', () => {
+describe('getStationInfo function', () => {
   const config = {
-    sncfApiUrl: 'https://ressources.data.sncf.com/api/records/1.0/',
-    debug: true,
+    sncfApiUrl: 'https://url/',
   };
 
-  // it('should return label when station exists', () => {
-  //   // given
-  //   const label = 'Lazare';
-  //   // when
-  //   const actual = getStationInfoByLabel(label, config);
-  //   // then
-  //   expect(actual.code_uic).toEqual('Paris Saint-Lazare');
-  // });
-
-  it('should return null when station does not exist', () => {
-    // given-when
-    const actual = getStationInfoByLabel('azerty', config);
+  it('should return null in test context', () => {
+    // given
+    const query = 'Lazare';
+    // when
+    const actual = getStationInfo(query, config);
     // then
     expect(actual).toBeNull();
   });
