f722153745a5e73131be2706caeceed17c6a3e72
djey <j.canler@gmail.com>
2017-06-29 00:06:49 +0200

    refactor(xml): change xml to json conversion, use xml2s

--- src/support/xml.js
+++ src/support/xml.js
@@ -1,55 +1,19 @@
+import { parseString } from 'xml2js';
+
+const options = { explicitArray: false }; 
+
 /**
- * Modified version from here: http://davidwalsh.name/convert-xml-json
- * @param {any} xml DOM-parsed XML object
- * @returns corresponding JS object
+ * @param {String} xml XML string
+ * @returns {Object} corresponding JS object
  */
 function xmlToJson(xml) {
 	// Create the return object
-  let obj = {};
+  let obj = null;
 
-  if (xml.nodeType == 1) { // element
-		// do attributes
-    if (xml.attributes.length) {
-      obj['@attributes'] = {};
-      for (let j = 0; j < xml.attributes.length; j++) {
-        const attribute = xml.attributes.item(j);
-        const { nodeName, nodeValue } = attribute;
-        obj['@attributes'][nodeName] = nodeValue;
-      }
-    }
-  } else if (xml.nodeType == 3) { // text
-    obj = xml.nodeValue;
-  }
+  parseString(xml, options, (err, result) => {
+    if (!err) obj = result;
+  });
 
-	// do children
-	// If just one text node inside
-  if (xml.hasChildNodes()
-    && xml.childNodes.length === 1
-    && xml.childNodes[0].nodeType === 3) {
-    const [ childNode ] = xml.childNodes; 
-    // Text node with attributes?
-    if (obj['@attributes']) {
-      obj['@attributes'].text = childNode.nodeValue;
-    } else {
-      obj = childNode.nodeValue;
-    }    
-  }
-  else if (xml.hasChildNodes()) {
-    for(let i = 0; i < xml.childNodes.length; i++) {
-      const item = xml.childNodes.item(i);
-      const { nodeName } = item;
-      if (typeof(obj[nodeName]) === 'undefined') {
-        obj[nodeName] = xmlToJson(item);
-      } else {
-        if (typeof(obj[nodeName].push) === 'undefined') {
-          const old = obj[nodeName];
-          obj[nodeName] = [];
-          obj[nodeName].push(old);
-        }
-        obj[nodeName].push(xmlToJson(item));
-      }
-    }
-  }
   return obj;
 }
 
--- src/support/xml.spec.js
+++ src/support/xml.spec.js
@@ -3,12 +3,20 @@
 import xmlToJson from './xml';
 
 describe('xmlToJson function', () => {
+  it('should return null when unparsable data', () => {
+    // given
+    const xml = '<guestbook';
+    // when
+    const actual = xmlToJson(xml);
+    // then
+    expect(actual).toBeNull();
+  });
+
   it('should return correct JSON (simple case)', () => {
     // given
     const xml = '<guestbook><guest><fname>Terje</fname><lname>Beck</lname></guest><guest><fname>Jan</fname><lname>Refsnes</lname></guest></guestbook>';
-    const xmlDoc = new DOMParser().parseFromString(xml, 'text/xml');
     // when
-    const actual = xmlToJson(xmlDoc);
+    const actual = xmlToJson(xml);
     // then
     expect(actual).toEqual({
       guestbook: {
@@ -25,23 +33,22 @@ describe('xmlToJson function', () => {
 
   it('should return correct JSON (complex case)', () => {
     // given
-    const xml = '<?xml version="1.0" encoding="UTF-8"?><passages gare="87382002"><train><date mode="R">20/06/2017 12:46</date><num>135140</num><miss>POPI</miss><term>87384008</term><etat>Retardé</etat></train><train><date mode="R">20/06/2017 13:41</date><num>134626</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 13:41</date><num>135183</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 13:45</date><num>135162</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 13:50</date><num>134625</num><miss>SEBU</miss><term>87382481</term></train><train><date mode="R">20/06/2017 13:51</date><num>135189</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 13:53</date><num>135164</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 13:56</date><num>134630</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:01</date><num>135193</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 14:03</date><num>135168</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:05</date><num>134629</num><miss>SEBU</miss><term>87382481</term></train><train><date mode="R">20/06/2017 14:11</date><num>134632</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:11</date><num>135195</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 14:13</date><num>135172</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:20</date><num>134631</num><miss>SEBU</miss><term>87382481</term></train><train><date mode="R">20/06/2017 14:21</date><num>135203</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 14:23</date><num>135174</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:26</date><num>134636</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:31</date><num>135205</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 14:33</date><num>135182</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:35</date><num>134635</num><miss>SEBU</miss><term>87382481</term></train><train><date mode="R">20/06/2017 14:41</date><num>134638</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:41</date><num>135209</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 14:43</date><num>135184</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:50</date><num>134637</num><miss>SEBU</miss><term>87382481</term></train><train><date mode="R">20/06/2017 14:51</date><num>135213</num><miss>NOPE</miss><term>87386318</term></train><train><date mode="R">20/06/2017 14:53</date><num>135188</num><miss>POPI</miss><term>87384008</term></train><train><date mode="R">20/06/2017 14:56</date><num>134642</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 15:01</date><num>135219</num><miss>NOPE</miss><term>87386318</term></train></passages>';
-    const xmlDoc = new DOMParser().parseFromString(xml, 'text/xml');
+    const xml = '<?xml version="1.0" encoding="UTF-8"?><passages gare="87382002"><train><date mode="R">20/06/2017 12:46</date><num>135140</num><miss>POPI</miss><term>87384008</term><etat>Retardé</etat></train><train><date mode="R">20/06/2017 13:41</date><num>134626</num><miss>PEBU</miss><term>87384008</term></train><train><date mode="R">20/06/2017 13:41</date><num>135183</num><miss>NOPE</miss><term>87386318</term></train></passages>';
     // when
-    const actual = xmlToJson(xmlDoc);
+    const actual = xmlToJson(xml);
     // then
     expect(actual).toEqual({
       passages:{
-        '@attributes':{
+        '$':{
           gare:'87382002',
         },
         train:[
           {
             date:{
-              '@attributes':{
+              '$':{
                 mode:'R',
-                text:'20/06/2017 12:46',
               },
+              _:'20/06/2017 12:46',
             },
             etat:'Retardé',
             miss:'POPI',
@@ -50,10 +57,10 @@ describe('xmlToJson function', () => {
           },
           {
             date:{
-              '@attributes':{
+              '$':{
                 mode:'R',
-                text:'20/06/2017 13:41',
               },
+              _:'20/06/2017 13:41',
             },
             miss:'PEBU',
             num:'134626',
@@ -61,299 +68,13 @@ describe('xmlToJson function', () => {
           },
           {
             date:{
-              '@attributes':{
+              '$':{
                 mode:'R',
-                text:'20/06/2017 13:41',
               },
+              _:'20/06/2017 13:41',
             },
             miss:'NOPE',
             num:'135183',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 13:45',
-              },
-            },
-            miss:'POPI',
-            num:'135162',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 13:50',
-              },
-            },
-            miss:'SEBU',
-            num:'134625',
-            term:'87382481',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 13:51',
-              },
-            },
-            miss:'NOPE',
-            num:'135189',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 13:53',
-              },
-            },
-            miss:'POPI',
-            num:'135164',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 13:56',
-              },
-            },
-            miss:'PEBU',
-            num:'134630',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:01',
-              },
-            },
-            miss:'NOPE',
-            num:'135193',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:03',
-              },
-            },
-            miss:'POPI',
-            num:'135168',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:05',
-              },
-            },
-            miss:'SEBU',
-            num:'134629',
-            term:'87382481',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:11',
-              },
-            },
-            miss:'PEBU',
-            num:'134632',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:11',
-              },
-            },
-            miss:'NOPE',
-            num:'135195',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:13',
-              },
-            },
-            miss:'POPI',
-            num:'135172',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:20',
-              },
-            },
-            miss:'SEBU',
-            num:'134631',
-            term:'87382481',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:21',
-              },
-            },
-            miss:'NOPE',
-            num:'135203',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:23',
-              },
-            },
-            miss:'POPI',
-            num:'135174',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:26',
-              },
-            },
-            miss:'PEBU',
-            num:'134636',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:31',
-              },
-            },
-            miss:'NOPE',
-            num:'135205',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:33',
-              },
-            },
-            miss:'POPI',
-            num:'135182',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:35',
-              },
-            },
-            miss:'SEBU',
-            num:'134635',
-            term:'87382481',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:41',
-              },
-            },
-            miss:'PEBU',
-            num:'134638',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:41',
-              },
-            },
-            miss:'NOPE',
-            num:'135209',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:43',
-              },
-            },
-            miss:'POPI',
-            num:'135184',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:50',
-              },
-            },
-            miss:'SEBU',
-            num:'134637',
-            term:'87382481',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:51',
-              },
-            },
-            miss:'NOPE',
-            num:'135213',
-            term:'87386318',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:53',
-              },
-            },
-            miss:'POPI',
-            num:'135188',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 14:56',
-              },
-            },
-            miss:'PEBU',
-            num:'134642',
-            term:'87384008',
-          },
-          {
-            date:{
-              '@attributes':{
-                mode:'R',
-                text:'20/06/2017 15:01',
-              },
-            },
-            miss:'NOPE',
-            num:'135219',
             term:'87386318',
           },
         ],
