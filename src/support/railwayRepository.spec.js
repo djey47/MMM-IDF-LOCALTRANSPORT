8306c90c88384e8ed100d0d50ac70ae642508448
jerome.canler <jerome.canler.external@airbus.com>
2019-09-17 16:32:22 +0200

    test(support): remove unhandled promise rejection warnings

--- src/support/railwayRepository.spec.js
+++ src/support/railwayRepository.spec.js
@@ -13,15 +13,8 @@ import {
 } from './cache';
 import { defaults } from './configuration';
 
-const mockThen = jest.fn();
-const mockAll = jest.fn(() => ({
-  then: () => mockThen(),
-}));
-const mockGet = jest.fn();
-jest.mock('axios', () => ({
-  get: (url, axiosConfig) => mockGet(url, axiosConfig),
-  all: () => mockAll(),
-}));
+jest.mock('axios');
+const mockAxios = require('axios');
 
 const mockResolve = jest.fn();
 
@@ -30,159 +23,173 @@ const config = {
   apiSncfData: 'https://foo/bar',
 };
 
-beforeEach(() => {
-  mockGet.mockReset();
-  mockAll.mockReset();
-  mockResolve.mockReset();
-  resetInfoCache();
-});
+describe('railway repository functions', () => {
+  beforeEach(() => {
+    mockAxios.get.mockReset();
+    mockAxios.all.mockReset();
+    mockResolve.mockReset();
 
-describe('getStationInfo function', () => {
-  it('should return Promise', () => {
-    // given
-    const query = {
-      index: 0,
-      stationValue: 'Lazare',
-      destinationValue: 'St Cloud',
-    };
-    // when
-    const actual = getStationInfo(query, config);
-    // then
-    expect(actual).toBeDefined();
-    expect(mockAll).toHaveBeenCalled();
-    expect(mockGet).toHaveBeenCalledTimes(2);
-    expect(mockGet).toHaveBeenCalledWith('https://foo/barsearch?q=St%20Cloud&dataset=sncf-gares-et-arrets-transilien-ile-de-france&sort=libelle', axiosConfig);
-    expect(mockGet).toHaveBeenCalledWith('https://foo/barsearch?q=Lazare&dataset=sncf-gares-et-arrets-transilien-ile-de-france&sort=libelle', axiosConfig);
+    mockAxios.get.mockImplementation((url, axiosConfig) => ({
+      url,
+      axiosConfig,
+      then: () => {},
+    }));
+    mockAxios.all.mockImplementation((axiosPromises) => ({
+      axiosPromises,
+      then: () => {},
+    }));
+
+    resetInfoCache();
   });
 
-  it('should return Promise without API call if response already in cache', () => {
-    // given
-    const query = {
-      index: 0,
-      stationValue: 'Lazare',
-    };
-    putInfoInCache('Lazare', { code_uic: 'UIC1', libelle: 'L1' });
-    // when
-    const actual = getStationInfo(query, config);
-    // then
-    expect(actual).toBeDefined();
-    expect(mockAll).not.toHaveBeenCalled();
-    expect(mockGet).not.toHaveBeenCalled();
-  });
-});
-
-describe('getAllStationInfo function', () => {
-  it('should return Promise', () => {
-    // given
-    const queries = [{
-      index: 0,
-      stationValue: 'Lazare',
-      destinationValue: 'St Cloud',
-    }, {
-      index: 1,
-      stationValue: 'Chaville',
-      destinationValue: 'Versailles',
-    }];
-    // when
-    const actual = getAllStationInfo(queries, config);
-    // then
-    expect(actual).toBeDefined();
-    expect(mockGet).toHaveBeenCalledTimes(4);
-  });
-});
-
-describe('handleInfoResponsesOnSuccess function', () => {
-  it('should resolve values properly for both station and destination, and update cache', () => {
-    // given
-    const responses = [
-      {
-        data: {
-          records: [
-            {
-              fields: { libelle: 'becon', code_uic: 'UIC1' },
-            },
-          ],
-        },
-      },
-      {
-        data: {
-          records: [
-            {
-              fields: { libelle: 'st cloud', code_uic: 'UIC2' },
-            },
-          ],
-        },
-      },
-    ];
-    const query = {
-      index: 0,
-      stationValue: 'Becon',
-      destinationValue: 'St Cloud',
-    };
-    // when
-    handleInfoResponsesOnSuccess(responses, mockResolve, query, config.debug);
-    // then
-    expect(mockResolve).toHaveBeenCalledWith({
-      index: 0,
-      stationInfo: {
-        libelle: 'becon',
-        code_uic: 'UIC1',
-      },
-      destinationInfo: {
-        libelle: 'st cloud',
-        code_uic: 'UIC2',
-      },
+  describe('getStationInfo function', () => {
+    it('should return Promise', () => {
+      // given
+      const query = {
+        index: 0,
+        stationValue: 'Lazare',
+        destinationValue: 'St Cloud',
+      };
+      // when
+      const actual = getStationInfo(query, config);
+      // then
+      expect(actual).toBeDefined();
+      expect(mockAxios.all).toHaveBeenCalled();
+      expect(mockAxios.get).toHaveBeenCalledTimes(2);
+      expect(mockAxios.get).toHaveBeenCalledWith('https://foo/barsearch?q=St%20Cloud&dataset=sncf-gares-et-arrets-transilien-ile-de-france&sort=libelle', axiosConfig);
+      expect(mockAxios.get).toHaveBeenCalledWith('https://foo/barsearch?q=Lazare&dataset=sncf-gares-et-arrets-transilien-ile-de-france&sort=libelle', axiosConfig);
     });
-    expect(getInfoFromCache('Becon')).toEqual({ libelle: 'becon', code_uic: 'UIC1' });
-    expect(getInfoFromCache('St Cloud')).toEqual({ libelle: 'st cloud', code_uic: 'UIC2' });
-  });
 
-  it('should resolve values properly for station only, and update cache', () => {
-    // given
-    const responses = [
-      {
-        data: {
-          records: [
-            {
-              fields: { libelle: 'becon', code_uic: 'UIC1' },
-            },
-          ],
-        },
-      },
-    ];
-    const query = {
-      index: 0,
-      stationValue: 'Becon',
-    };
-    // when
-    handleInfoResponsesOnSuccess(responses, mockResolve, query, config.debug);
-    // then
-    expect(mockResolve).toHaveBeenCalledWith({
-      index: 0,
-      stationInfo: {
-        libelle: 'becon',
-        code_uic: 'UIC1',
-      },
-      destinationInfo: null,
+    it('should return Promise without API call if response already in cache', () => {
+      // given
+      const query = {
+        index: 0,
+        stationValue: 'Lazare',
+      };
+      putInfoInCache('Lazare', { code_uic: 'UIC1', libelle: 'L1' });
+      // when
+      const actual = getStationInfo(query, config);
+      // then
+      expect(actual).toBeDefined();
+      expect(mockAxios.all).not.toHaveBeenCalled();
+      expect(mockAxios.get).not.toHaveBeenCalled();
     });
-    expect(getInfoFromCache('Becon')).toEqual({ libelle: 'becon', code_uic: 'UIC1' });
   });
 
-  it('should resolve to null when wrong response', () => {
-    // given
-    const responses = [
-      {
-        data: { records: []},
-      },
-    ];
-    const query = {
-      index: 0,
-      stationValue: 'Becon',
-    };
-    // when
-    handleInfoResponsesOnSuccess(responses, mockResolve, query, config.debug);
-    // then
-    expect(mockResolve).toHaveBeenCalledWith(null);
+  describe('getAllStationInfo function', () => {
+    it('should return Promise', () => {
+      // given
+      const queries = [{
+        index: 0,
+        stationValue: 'Lazare',
+        destinationValue: 'St Cloud',
+      }, {
+        index: 1,
+        stationValue: 'Chaville',
+        destinationValue: 'Versailles',
+      }];
+      // when
+      const actual = getAllStationInfo(queries, config);
+      // then
+      expect(actual).toBeDefined();
+      expect(mockAxios.get).toHaveBeenCalledTimes(4);
+      expect(mockAxios.all).toHaveBeenCalledTimes(2);
+    });
+  });
 
+  describe('handleInfoResponsesOnSuccess function', () => {
+    it('should resolve values properly for both station and destination, and update cache', () => {
+      // given
+      const responses = [
+        {
+          data: {
+            records: [
+              {
+                fields: { libelle: 'becon', code_uic: 'UIC1' },
+              },
+            ],
+          },
+        },
+        {
+          data: {
+            records: [
+              {
+                fields: { libelle: 'st cloud', code_uic: 'UIC2' },
+              },
+            ],
+          },
+        },
+      ];
+      const query = {
+        index: 0,
+        stationValue: 'Becon',
+        destinationValue: 'St Cloud',
+      };
+      // when
+      handleInfoResponsesOnSuccess(responses, mockResolve, query, config.debug);
+      // then
+      expect(mockResolve).toHaveBeenCalledWith({
+        index: 0,
+        stationInfo: {
+          libelle: 'becon',
+          code_uic: 'UIC1',
+        },
+        destinationInfo: {
+          libelle: 'st cloud',
+          code_uic: 'UIC2',
+        },
+      });
+      expect(getInfoFromCache('Becon')).toEqual({ libelle: 'becon', code_uic: 'UIC1' });
+      expect(getInfoFromCache('St Cloud')).toEqual({ libelle: 'st cloud', code_uic: 'UIC2' });
+    });
+
+    it('should resolve values properly for station only, and update cache', () => {
+      // given
+      const responses = [
+        {
+          data: {
+            records: [
+              {
+                fields: { libelle: 'becon', code_uic: 'UIC1' },
+              },
+            ],
+          },
+        },
+      ];
+      const query = {
+        index: 0,
+        stationValue: 'Becon',
+      };
+      // when
+      handleInfoResponsesOnSuccess(responses, mockResolve, query, config.debug);
+      // then
+      expect(mockResolve).toHaveBeenCalledWith({
+        index: 0,
+        stationInfo: {
+          libelle: 'becon',
+          code_uic: 'UIC1',
+        },
+        destinationInfo: null,
+      });
+      expect(getInfoFromCache('Becon')).toEqual({ libelle: 'becon', code_uic: 'UIC1' });
+    });
+
+    it('should resolve to null when wrong response', () => {
+      // given
+      const responses = [
+        {
+          data: { records: []},
+        },
+      ];
+      const query = {
+        index: 0,
+        stationValue: 'Becon',
+      };
+      // when
+      handleInfoResponsesOnSuccess(responses, mockResolve, query, config.debug);
+      // then
+      expect(mockResolve).toHaveBeenCalledWith(null);
+
+    });
   });
 });
